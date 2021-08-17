2c068d976ba7bb8e6756081fcb1badf62aad6c44
jyno <jinho9613@gmail.com>
2021-08-17 22:59:21 +0900

    user 사용 init

---
+++ database.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, event
+from sqlalchemy.engine import Engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import os
+from pathlib import Path
+
+path = Path(os.path.realpath(__file__)).parent.parent.absolute()
+
+SQLALCHAMY_DATABASE_URL = f'sqlite:////{path}/AuditLog.db'
+# Test code 에서도 사용하기 때문에 절대 경로로 지정해 준다
+engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={"check_same_thread": False}, pool_pre_ping=True)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, )
+
+Base = declarative_base()
+
+
+# sqlite CASCADE 용
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ token.py
@@ -0,0 +1,54 @@
+from datetime import datetime, timedelta
+
+from fastapi import Depends
+from fastapi_jwt_auth import AuthJWT
+
+from jose import JWTError, jwt
+from pydantic import BaseModel
+
+from . import schemas
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+# Generate Refresh Token Setting
+# in production you can use Settings management
+# from pydantic to get secret key from .env
+class Settings(BaseModel):
+    authjwt_secret_key: str = SECRET_KEY
+
+
+# callback to get your configuration
+@AuthJWT.load_config
+def get_config():
+    return Settings()
+# Generate Refresh Token Setting
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    # Use create_access_token() and create_refresh_token() to create our
+    # access and refresh tokens
+    # access_token = AuthJWT.create_access_token(subject=to_encode)
+    # refresh_token = AuthJWT.create_refresh_token(subject=to_encode)
+    # return {"access_token": access_token, "refresh_token": refresh_token}
+
+    return encoded_jwt
+
+
+def verify_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = schemas.user.TokenData(username=username)
+        return token_data
+    except JWTError:
+        raise credentials_exception

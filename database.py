43f27d517d3e317c40253f12e37ac78634e11418
jyno <jinho9613@gmail.com>
2021-11-25 20:38:33 +0900

    init gitignore cors

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 path = Path(os.path.realpath(__file__)).parent.absolute()
 
-SQLALCHAMY_DATABASE_URL = f'sqlite:////{path}/ksd-naver-ocr-server.db'
+SQLALCHAMY_DATABASE_URL = f'sqlite:////{path}/ocr-server.db'
 # Test code 에서도 사용하기 때문에 절대 경로로 지정해 준다
 engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={"check_same_thread": False}, pool_pre_ping=True)
 
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from models import manage
 from routers import ocr, user
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 manage.create_all()
 

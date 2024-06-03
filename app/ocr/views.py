26c4996f0efac25f9657aa3dfff60c4cb0da3d89
devjyno <devjyno96@gmail.com>
2024-06-03 22:00:43 +0900

    feat: add general ocr and add docs

--- app/ocr/views.py
+++ app/ocr/views.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.category.schemas import OCRShowV3, RequestOCRV3
 from app.database.core import get_db
+from app.naver_clova_ocr.schemas import ClovaOCRResponseV3
 from app.ocr.services import find_best_matching_category, process_category_ocr, process_general_ocr
 
 ocr_router_v4 = APIRouter(prefix="/v4")
@@ -10,7 +11,84 @@ ocr_router_v4 = APIRouter(prefix="/v4")
 
 @ocr_router_v4.post("/ocr", status_code=status.HTTP_201_CREATED, response_model=OCRShowV3)
 async def process_image_view(request_body: RequestOCRV3, db_session: Session = Depends(get_db)):
-    """이미지 URL을 받아 OCR 처리를 합니다."""
+    """
+    ---
+
+    # Request Body
+        {
+          "image_url": "string", # public 접근 가능한 image url
+          "file_name_extension": "string" # image url 의 파일 확장자
+        }
+
+    ---
+
+    # Response Body
+
+        {
+          "category": "string", # 아래 작성되어있는 분류 항목의 카테고리
+          "domain_name": "string", # 아래 작성되어있는 분류 항목 domain_name
+          "result": json # Naver Clova OCR 결과 - 참고 문헌 1 참고
+        }
+
+    ---
+
+    # 분류 항목
+
+        [
+          {
+            "domain_description": "0 처방전",
+            "domain_name": "Prescription",
+            "category": "PR",
+          },
+          {
+            "domain_description": "1 의학적검진",
+            "domain_name": "Medical Examination",
+            "category": "ME",
+          },
+          {
+            "domain_description": "2 인지및지능검사",
+            "domain_name": "Perception and Intelligence Test",
+            "category": "PI",
+          },
+          {
+            "domain_description": "3 사회성 및 자폐검사",
+            "domain_name": "Sociality & Autism Test",
+            "category": "SA",
+          },
+          {
+            "domain_description": "4 언어평가",
+            "domain_name": "Language Tests",
+            "category": "LR",
+          },
+          {
+            "domain_description": "5 주의력 및 신경인지검사",
+            "domain_name": "Attention and Neurocognitive Function Test",
+            "category": "AN",
+          },
+          {
+            "domain_description": "6 기질 성격 정서검사",
+            "domain_name": "Temperament and Character And Emotion Test",
+            "category": "TC",
+          },
+          {
+            "domain_description": "7 학습검사",
+            "domain_name": "Learning Test",
+            "category": "LT",
+          },
+          {
+            "domain_description": "8 발달 평가 결과",
+            "domain_name": "Infant Health Checkup",
+            "category": "IH",
+          }
+        ]
+
+    ---
+    # 참고 문헌
+
+    ## 1. Naver CLOVA OCR Custom API
+    - ### [https://api.ncloud-docs.com/docs/ai-application-service-ocr-ocr#v2-응답-바디](https://api.ncloud-docs.com/docs/ai-application-service-ocr-ocr#v2-응답-바디)
+
+    """
     general_result = await process_general_ocr(db_session, request_body.image_url, request_body.file_name_extension)
 
     if general_result is None:
@@ -23,3 +101,14 @@ async def process_image_view(request_body: RequestOCRV3, db_session: Session = D
     )
 
     return OCRShowV3(category=category.name, domain_name=category.description, result=category_result)
+
+
+@ocr_router_v4.post("/general-ocr", status_code=status.HTTP_201_CREATED, response_model=ClovaOCRResponseV3)
+async def process_genaral_image_view(request_body: RequestOCRV3, db_session: Session = Depends(get_db)):
+    """이미지 URL을 받아 General OCR 처리를 합니다. - Debug 용"""
+    general_result = await process_general_ocr(db_session, request_body.image_url, request_body.file_name_extension)
+
+    if general_result is None:
+        return "general ocr result is None -> Error"
+
+    return general_result
